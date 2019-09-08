5d0646afba18ce5c9cca0388757fe3fce113a38d
Daan Leenders <daanleenders@gmail.com>
2019-09-09 01:02:57 +0200

    Ran prettier on typography.js

--- src/utils/typography.js
+++ src/utils/typography.js
@@ -1,10 +1,10 @@
 import Typography from 'typography'
 import fairyGatesTheme from 'typography-theme-fairy-gates'
 
-fairyGatesTheme.blockMarginBottom = '0rem';
-fairyGatesTheme.fontSize = '0.85rem !important';
+fairyGatesTheme.blockMarginBottom = '0rem'
+fairyGatesTheme.fontSize = '0.85rem !important'
 // fairyGatesTheme.color = 'grey';
 
-const typography = new Typography(fairyGatesTheme);
+const typography = new Typography(fairyGatesTheme)
 
 export default typography
